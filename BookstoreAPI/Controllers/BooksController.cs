2621d23bb660f893c44d210788c45950b319ffed
Jose.Loria <loriafjose@gmail.com>
2025-02-12 12:35:27 -0600

    Adding documentation

--- BookstoreAPI/Controllers/BooksController.cs
+++ BookstoreAPI/Controllers/BooksController.cs
@@ -19,6 +19,11 @@ namespace BookstoreAPI.Controllers
             _unitOfWork = new UnitOfWork(context);
         }
 
+        //GET: /api/v1/Books/GetBooks
+        /// <summary>
+        /// Gets all books
+        /// </summary>
+        /// <returns>200 Code with List of books, if nothing found returns 404 Code Not Found</returns>
         [HttpGet("GetBooks")]
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
         {
@@ -30,7 +35,13 @@ namespace BookstoreAPI.Controllers
 
         }
 
-        [HttpGet("GetBookbyId")]
+        //GET: /api/v1/Books/GetBookById/id
+        /// <summary>
+        /// Gets a Book by id
+        /// </summary>
+        /// <param name="id">Integer represent Book id</param>
+        /// <returns>200 Code with Book info, if not found returns 404 Code Not found</returns>
+        [HttpGet("GetBookById")]
         public async Task<ActionResult<Book>> GetBookById(int id)
         {
             var result = await _unitOfWork.Books.GetById(id);
@@ -40,6 +51,12 @@ namespace BookstoreAPI.Controllers
             return Ok(result);
         }
 
+        //GET: /api/v1/Books/SearchBooks
+        /// <summary>
+        /// Returns a list of books based on the query used
+        /// </summary>
+        /// <param name="query">string representing the query need to be executed</param>
+        /// <returns>200 Code with list of books, if not found 404 Code Not Found, in case of error returns 400 Code Bad Request </returns>
         [HttpGet("SearchBooks")]
         public async Task<ActionResult<IEnumerable<Book>>> SearchBooks(string query)
         {
@@ -53,10 +70,17 @@ namespace BookstoreAPI.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
                 return BadRequest(ex.Message);
             }
         }
 
+        //POST: /api/v1/Books/AddBook
+        /// <summary>
+        /// Adds a new book 
+        /// </summary>
+        /// <param name="book">Book json object with the book information</param>
+        /// <returns>201 Code Created</returns>
         [HttpPost("AddBook")]
         public async Task<ActionResult> AddBook(Book book)
         {
@@ -70,10 +94,17 @@ namespace BookstoreAPI.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
                 return Conflict(ex);
             }
         }
 
+        //GET: /api/v1/Books/DeleteBook/id
+        /// <summary>
+        /// Deletes a book using the book id
+        /// </summary>
+        /// <param name="id">Integer representing Book id</param>
+        /// <returns>200 Code</returns>
         [HttpDelete("DeleteBook")]
         public async Task<ActionResult> DeleteBook(int id)
         {
@@ -86,6 +117,7 @@ namespace BookstoreAPI.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
                 return Conflict(ex);
             }
         }
